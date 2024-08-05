ed6c2b698a876526848f1b6c64cd8c68994f805f
MrAngalo <guilherme.carvalho7@hotmail.com>
2024-08-05 16:33:20 -0300

    added player movement

--- Assets/Inputs/MyPlayerScript.cs
+++ Assets/Inputs/MyPlayerScript.cs
@@ -1,25 +1,37 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class MyPlayerScript : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
+    public float speed = 1;
+    public float jumpForce = 1;
+
+    private float moveDir = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 velocity = rb.velocity;
+        velocity.x = moveDir;
+        rb.velocity = velocity;
     }
 
     protected void OnMove(InputValue value) {
-        Debug.Log(value.Get<float>());
+        moveDir = value.Get<float>() * speed;
     }
 
     protected void OnJump(InputValue value) {
-        
+        Vector2 velocity = rb.velocity;
+        velocity.y = jumpForce;
+        rb.velocity = velocity;
     }
 }
